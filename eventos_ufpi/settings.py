a4aa6a26f9c1791f00c9e9021616e521438032f9
Pedro Marques <junnior.pedro.pm@gmail.com>
2020-07-19 15:09:01 -0300

    adjusting local settings

--- eventos_ufpi/settings.py
+++ eventos_ufpi/settings.py
@@ -83,13 +83,6 @@ WSGI_APPLICATION = 'eventos_ufpi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -134,6 +127,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL = '/login'
+
+try:
+    from eventos_ufpi.local_settings import *
+except ImportError:
+    pass
 
 if not DEBUG:
     django_heroku.settings(locals())
